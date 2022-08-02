00d6fc1b34c7aeaadfc6f1689fbca4574f4d6837
moise <moisemlg90@gmail.com>
2022-08-02 11:53:47 +0200

    Migrate generated mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_092541) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_093205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entities", force: :cascade do |t|
     t.string "name"
