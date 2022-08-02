d6af47598c0264e241d0aaeae1fe0bd4ace7394d
moise <moisemlg90@gmail.com>
2022-08-02 04:14:24 +0200

    Add authaurization for user

--- app/controllers/splashes_controller.rb
+++ app/controllers/splashes_controller.rb
@@ -1,5 +1,6 @@
 class SplashesController < ApplicationController
   before_action :set_splash, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /splashes or /splashes.json
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_162240) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_190958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "homes", force: :cascade do |t|
+  create_table "splashes", force: :cascade do |t|
     t.string "index"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
