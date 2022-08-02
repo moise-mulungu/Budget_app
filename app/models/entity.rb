1de299c437592b3b38406827bc4eb396f5cb490d
moise <moisemlg90@gmail.com>
2022-08-02 12:34:17 +0200

    Set associations between user, group and entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,4 @@
 class Entity < ApplicationRecord
+    belongs_to :users
+    has_many_and_belongs_to :groups
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+    belongs_to :users
+    has_many_and_belongs_to :entities
 end
