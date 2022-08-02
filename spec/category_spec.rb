ec388d50eb32cf8cd8e52b940199b1053a0e948e
moise <moisemlg90@gmail.com>
2022-08-02 18:34:49 +0200

    Add unit test for category

---
+++ spec/category_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Category', type: :request do
+    category = Category.new(user_id: 1, name: 'Category', icon: 'icon')
+    category.save
+    user = User.new(email: '@gmail.com', password: '123456')
+    user.save
+    it 'should create a category' do
+        post "/categories"
+        expect(response).to have_http_status(:created)
+    end
+    it 'should get all categories' do
+        get '/categories'
+        expect(response).to have_http_status(:ok)
+    end
+    it 'should get a category' do
+        get "/categories/#{category.id}"
+        expect(response).to have_http_status(:ok)
+    end
+    it 'should update a category' do
+        put "/categories/#{category.id}", params: { category: { name: 'Category2' } }
+        expect(response).to have_http_status(:updated)
+    end
+    it 'should delete a category' do
+        delete "/categories/#{category.id}"
+        expect(response).to have_http_status(:no_content)
+    end
+end
