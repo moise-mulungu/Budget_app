e701bfd514b114fc9880df840858e1f6122c5991
moise <moisemlg90@gmail.com>
2022-08-02 11:22:55 +0200

    Generate a foreign key for group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_083917) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_092118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "splashes", force: :cascade do |t|
     t.string "index"
@@ -32,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_083917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
 end
