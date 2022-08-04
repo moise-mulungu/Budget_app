2a2ec31627331bfb243ffe47ba3943bd8426d37e
moise <moisemlg90@gmail.com>
2022-08-04 09:45:38 +0200

    Add validation to payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,10 @@
 class Payment < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :amount, presence: true
+  validates :category_id, presence: true
+  validates :user_id, presence: true
+  validates :name, presence: true
+
 end
